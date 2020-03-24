55b2e934cc57bb39aa8a9be756d0c563579a1d7f
dhkaneda <ryqz.ig@gmail.com>
2020-03-24 11:09:07 -0700

    Install esLint with AirBnB style guide

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const app = express();
 const port = 5001;
 
 app.listen(port, () => {
   console.log(`Proxy is listening on ${port}`);
-})
+});
 
 app.use(express.static('./dist'));
